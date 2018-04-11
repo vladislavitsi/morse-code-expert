2d0ab69e1bde93fa6fe142051959413f335b16c7
Vladislav Kleschenko <vladislavitsi@gmail.com>
2018-04-11 23:05:54 +0300

    Initial Commit

---
+++ morse-code-expert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  morse-code-expert
+//
+//  Created by Vladislav Kleschenko on 4/11/18.
+//  Copyright © 2018 Vladislav Kleschenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ morse-code-expert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  morse-code-expert
+//
+//  Created by Vladislav Kleschenko on 4/11/18.
+//  Copyright © 2018 Vladislav Kleschenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
